7b8bb2af6b84f01ce8a361e77350d7f65c18fb79
Mark Easley <measley@ti.com>
2014-01-14 17:17:05 -0600

    Baseline Release v1.0
    Signed-off-by: Mark Easley <measley@ti.com>

--- energia/footer.php
+++ energia/footer.php
@@ -14,11 +14,12 @@
 			<div id="pagefooter">
 				<em>Corrections, suggestions, and new documentation should be posted to the <a href="http://forum.43oh.com/forum/28-energia/" rel="nofollow">Forum</a>.</em><br /><br />
 				The text of the Energia getting started and reference guides are licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/" rel="nofollow">Creative Commons Attribution-ShareAlike 3.0 License</a>. Energia reference is based on Arduino reference. Code samples in the guide are released into the public domain.<br /><br />
+				Energia sketches are C/C++ based and compiled with the open-source compiler MSPGCC. The Energia language comes from <a href="http://wiring.org.co/" rel="nofollow">Wiring</a>. The Energia environment is based on <a href="http://processing.org/" rel="nofollow">Processing</a> and includes modifications made by Wiring.<br /><br />
 				&copy;Energia <br />
 				<?php do_action( 'energia_credits' ); ?>
 				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'energia' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'energia' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( __( 'Theme: %1$s by %2$s.', 'energia' ), 'Energia', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'energia' ), '<a href="https://github.com/measley/energia-theme" rel="designer">Energia</a>', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
